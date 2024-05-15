999080bc68eff5ceca64d875dc0d73fe18b51b06
Ronak9595 <ronakgupta9595@gmail.com>
2024-05-15 21:17:50 +0530

    Second Day

--- app/src/main/java/com/myapplication/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/myapplication/MainActivity.java
@@ -3,8 +3,36 @@ package com.myapplication.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    int activePlayer=0;
+    int[] gameState={2,2,2,2,2,2,2,2,2};
+    int[][] winPositions={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    public void playerTap(View view)
+    {
+        ImageView img=(ImageView) view;
+        int tappedImage=Integer.parseInt(img.getTag().toString());
+        if(gameState[tappedImage]==2)
+        {
+            gameState[tappedImage]=activePlayer;
+            img.setTranslationY(-1000f);
+            if(activePlayer==0)
+            {
+                img.setImageResource(R.drawable.cross);
+                activePlayer=1;
+            }
+            else
+            {
+              img.setImageResource(R.drawable.zero);
+              activePlayer=0;
+            }
+
+        }
+        img.animate().translationYBy(1000f).setDuration(300);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
